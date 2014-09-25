713796dc82bb4c65f59f548282ea75355de8c26c
zamb <zamb@ericpol.com>
2014-09-25 16:00:52 +0200

    again with working test2

--- src/MyUnitTest.java
+++ src/MyUnitTest.java
@@ -9,7 +9,7 @@ public class MyUnitTest {
 
         String result = myUnit.concatenate("one", "two");
 
-        assertEquals("onetwo", result);
+        assertEquals("onetwofour", result);
 
     }
 }
